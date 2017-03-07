519edde9fa74c44991d9e2c756eed75cfb67a6c2
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-08 00:04:03 +0900

    add batch

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module WifiApi
       request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
+
+    config.autoload_paths += Dir["#{config.root}/lib"]
   end
 end
---
+++ lib/tasks/batch.rb
@@ -0,0 +1,5 @@
+class Tasks::Batch
+  def self.execute
+    p "Hello world"
+  end
+end

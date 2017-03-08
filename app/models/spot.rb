a26fa6a8966832c7121711de7b6fd2554fb03fad
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-08 21:44:35 +0900

    add distance function

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,45 @@
 class Spot < ApplicationRecord
+  EARTH_RADIUS = 6378150
+
+  def self.getDistance(lat1, lng1, lat2, lng2) # 単位:m
+    EARTH_RADIUS *
+    Math.acos(Math.cos(radians(lat1)) *
+    Math.cos(radians(lat2)) *
+    Math.cos(radians(lng2) - radians(lng1)) +
+    Math.sin(radians(lat1)) *
+    Math.sin(radians(lat2)))
+  end
+
+  def self.move(lat, lng, distance, heading)
+    # 緯線上の移動距離
+    latitude_distance = distance * Math.cos(heading * Math::PI / 180)
+    # 1mあたりの緯度
+    earth_circle = 2 * Math::PI * EARTH_RADIUS
+    latitude_per_meter = 360 / earth_circle
+    # 緯度の変化量
+    latitude_delta = latitude_distance * latitude_per_meter
+    new_latitude = lat + latitude_delta
+    # 経線上の移動距離
+    longitude_distance = distance * Math.sin(heading * Math::PI / 180)
+    # 1mあたりの経度
+    earth_radius_at_longitude = EARTH_RADIUS * Math.cos(new_latitude * Math::PI / 180)
+    earth_circle_at_longitude = 2 * Math::PI * earth_radius_at_longitude
+    longitude_per_meter = 360 / earth_circle_at_longitude
+    # 経度の変化量
+    longitude_delta = longitude_distance * longitude_per_meter
+    {lat: new_latitude, lng: lng + longitude_delta}
+  end
+
+  def self.getRange(lat, lng, distance)
+    lat1 = move(lat, lng, distance, 0)[:lat]
+    lat2 = move(lat, lng, distance, 180)[:lat]
+    lng1 = move(lat, lng, distance, 90)[:lng]
+    lng2 = move(lat, lng, distance, 270)[:lng]
+    {max_lat: lat1, min_lat: lat2, max_lng: lng1, min_lng: lng2}
+  end
+
+  private
+    def self.radians(deg)
+      deg * Math::PI / 180
+    end
 end

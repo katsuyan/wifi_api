43124120a2161d8c754fb53d788dd9e5f9580319
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-09 20:56:27 +0900

    add model class method test

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
   describe 'spot' do
+    it "radians" do
+      expect(Spot.__send__(:radians, 180)).to eq 3.141592653589793
+    end
+
     it "500メートル以内(デフォルト)取得" do
       8.times{ create(:spot) }
-      expect(Spot.getInsideAll(35.0, 140.0).count).to eq 6
+      expect(Spot.getInsideAll(35.0, 140.0, 500, 100).count).to eq 6
     end
   end
 end

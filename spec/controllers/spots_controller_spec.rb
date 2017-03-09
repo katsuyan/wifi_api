5b0c49e373b9acb97f5494771b077685f793a3de
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-10 08:11:56 +0900

    add test

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -2,14 +2,26 @@ require 'rails_helper'
 
 RSpec.describe SpotsController, type: :controller do
   describe 'GET #show' do
-    it "ステータスコードとして200が返ること" do
-      get :spots, lat: 0, lng: 0
-      expect(response.status).to eq 200
-    end
+    describe 'params lat and lng' do
+      it "ステータスコードとして200が返ること" do
+        get :spots, params: {lat: 0, lng: 0}
+        expect(response.status).to eq 200
+      end
 
-    it "ステータスコードとして400が返ること" do
-      get :spots
-      expect(response.status).to eq 400
+      it "ステータスコードとして400が返ること" do
+        get :spots
+        expect(response.status).to eq 400
+      end
+
+      it "ステータスコードとして400が返ること" do
+        get :spots, params: {lng: 0}
+        expect(response.status).to eq 400
+      end
+
+      it "ステータスコードとして400が返ること" do
+        get :spots, params: {lat: 0}
+        expect(response.status).to eq 400
+      end
     end
   end
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  describe 'spot' do
-    it "radians" do
-      expect(Spot.__send__(:radians, 180)).to eq 3.141592653589793
-    end
-
+  describe '.getInsideAll' do
     it "500メートル以内(デフォルト)取得" do
       8.times{ create(:spot) }
       expect(Spot.getInsideAll(35.0, 140.0, 500, 100).count).to eq 6
     end
+  end
+
+  describe '.radians' do
+    it "ラジアンの180がMath::PIであること" do
+      expect(Spot.__send__(:radians, 180)).to eq Math::PI
+    end
+  end
+
+  describe '.move' do
+
+  end
+
+  describe '.getDistance' do
+
+  end
+
+  describe '.getRange' do
+
   end
 end

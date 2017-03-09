53d44fb2a72fcd17bbcc5d696446c7c3c5e2da57
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-10 08:52:42 +0900

    clear Spot.move test

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -20,47 +20,48 @@ RSpec.describe Spot, type: :model do
     let(:lat) { 35.0 }
     let(:lng) { 140.0 }
     let(:distance) { 500 }
+    let(:move) { Spot.__send__(:move, lat, lng, distance, direction) }
     describe '0度方向(北)への移動' do
       let(:direction) { 0 }
       it 'lngは変化しないこと' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lng]).to eq lng
+        expect(move[:lng]).to eq lng
       end
 
       it 'latが増加すること' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lat]).to be > lat
+        expect(move[:lat]).to be > lat
       end
     end
 
     describe '180度方向(北)への移動' do
       let(:direction) { 180 }
       it 'lngは変化しないこと' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lng]).to eq lng
+        expect(move[:lng]).to eq lng
       end
 
       it 'latが減少すること' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lat]).to be < lat
+        expect(move[:lat]).to be < lat
       end
     end
 
     describe '90度方向(北)への移動' do
       let(:direction) { 90 }
       it 'latは変化しないこと' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lat]).to eq lat
+        expect(move[:lat]).to eq lat
       end
 
       it 'lngが増加すること' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lng]).to be > lng
+        expect(move[:lng]).to be > lng
       end
     end
 
     describe '270度方向(北)への移動' do
       let(:direction) { 270 }
       it 'latは変化しないこと' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lat]).to eq lat
+        expect(move[:lat]).to eq lat
       end
 
       it 'lngが減少すること' do
-        expect(Spot.__send__(:move, lat, lng, distance, direction)[:lng]).to be < lng
+        expect(move[:lng]).to be < lng
       end
     end
   end

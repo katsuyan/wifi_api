669d360bfd2bedcefe6dd29f0863c9417abd8cf1
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-09 14:08:19 +0900

    add test

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe SpotsController, type: :controller do
   describe 'GET #show' do
     it "ステータスコードとして200が返ること" do
-      get :spots
+      get :spots, lat: 0, lng: 0
       expect(response.status).to eq 200
+    end
+
+    it "ステータスコードとして400が返ること" do
+      get :spots
+      expect(response.status).to eq 400
     end
   end
 end

c0f304fbde3623592b486a7c93dc5973fdc96b7c
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-10 09:52:16 +0900

    add Spot.getRange test

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -71,20 +71,20 @@ RSpec.describe Spot, type: :model do
     let(:lng1) { 140.0 }
     subject { Spot.__send__(:getDistance, lat1, lng1, lat2, lng2).to_i }
     context '距離が0くらいになること' do
-      let(:lat2) { 35.0 }
-      let(:lng2) { 140.0 }
+      let(:lat2) { lat1 }
+      let(:lng2) { lng1 }
       it { is_expected.to eq 0 }
     end
 
     describe '距離が500くらいになること' do
       context 'latだけ動かして' do
         let(:lat2) { 35.0045 }
-        let(:lng2) { 140.0 }
+        let(:lng2) { lng1 }
         it { is_expected.to eq 500 }
       end
 
       context 'lngだけ動かして' do
-        let(:lat2) { 35.0 }
+        let(:lat2) { lat1 }
         let(:lng2) { 140.00549 }
         it { is_expected.to eq 500 }
       end
@@ -98,6 +98,26 @@ RSpec.describe Spot, type: :model do
   end
 
   describe '.getRange' do
+    let(:lat) { 35 }
+    let(:lng) { 140 }
+    let(:range_hash) { {max_lat: max_lat, min_lat: min_lat, max_lng: max_lng, min_lng: min_lng} }
+    subject { Hash[Spot.__send__(:getRange, lat, lng, distance).map {|k,v| [k, v.round(8)] }] }
+    context 'max,min_lat max,min_lng がそれぞれlat,lngと同じになること' do
+      let(:distance) { 0 }
+      let(:max_lat) { lat }
+      let(:min_lat) { lat }
+      let(:max_lng) { lng }
+      let(:min_lng) { lng }
+      it { is_expected.to match(range_hash) }
+    end
 
+    context '500メートル離れた場合の範囲になること' do
+      let(:distance) { 500 }
+      let(:max_lat) { 35.00449157 }
+      let(:min_lat) { 34.99550843 }
+      let(:max_lng) { 140.00548319 }
+      let(:min_lng) { 139.99451681 }
+      it { is_expected.to match(range_hash) }
+    end
   end
 end

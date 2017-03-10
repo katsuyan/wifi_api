b903ad7f50a4e463e2f14877348aa1791c5e9775
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-10 11:40:18 +0900

    add spots controller test

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -2,28 +2,43 @@ require 'rails_helper'
 
 RSpec.describe SpotsController, type: :controller do
   describe 'GET #show' do
-    describe 'ステータスコードとして200が返ること' do
-      it 'lat,lngパラメータが存在する' do
-        get :spots, params: {lat: 0, lng: 0}
-        expect(response.status).to eq 200
+    let(:spots) { 10.times{ create(:spot) }}
+    let(:lat) { 35.0 }
+    let(:lng) {140.0 }
+    before { get :spots, params: {lat: lat, lng: lng}}
+
+    describe 'ステータスコードが仕様どおり返ること' do
+      describe 'ステータスコードとして200が返ること' do
+        it 'lat,lngパラメータが存在する' do
+          get :spots, params: {lat: lat, lng: lng}
+          expect(response.status).to eq 200
+        end
+      end
+
+      describe 'ステータスコードとして400が返ること' do
+        it 'lat,lngパラメータが存在しない' do
+          get :spots
+          expect(response.status).to eq 400
+        end
+
+        it 'latパラメータが存在しない' do
+          get :spots, params: {lng: lng}
+          expect(response.status).to eq 400
+        end
+
+        it 'lngパラメータが存在しない' do
+          get :spots, params: {lat: lat}
+          expect(response.status).to eq 400
+        end
       end
     end
 
-    describe 'ステータスコードとして400が返ること' do
-      it 'lat,lngパラメータが存在しない' do
-        get :spots
-        expect(response.status).to eq 400
-      end
+    it 'soptsテンプレートをrenderしていること' do
+      expect(response).to render_template :spots
+    end
 
-      it 'latパラメータが存在しない' do
-        get :spots, params: {lng: 0}
-        expect(response.status).to eq 400
-      end
-
-      it 'lngパラメータが存在しない' do
-        get :spots, params: {lat: 0}
-        expect(response.status).to eq 400
-      end
+    it '@spotsにオブジェクトのリストが格納されていること' do
+      expect(assigns(:spots).count).to be >= 0 
     end
   end
 end
